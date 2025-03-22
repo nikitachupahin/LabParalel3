61582cc1e855830a989b52aeef9e5a157601ba7e
nikitachupahin <nik123chupahin@gmail.com>
2025-03-22 18:25:04 +0200

    Add MyTask struct

---
+++ LabParalel3/MyTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LabParalel3
+{
+    struct MyTask
+    {
+        private double _sleepTime;
+        private int _id;
+        public int Id => _id;
+        public double SleepTime => _sleepTime;
+        public MyTask(int id, double sleepTime)
+        {
+            _id = id;
+            _sleepTime = sleepTime;
+        }
+    }
+}
