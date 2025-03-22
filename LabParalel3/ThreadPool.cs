ffca92d2e45fe087b60561f47aaaaf4c12393be3
nikitachupahin <nik123chupahin@gmail.com>
2025-03-22 17:34:46 +0200

    Add initial ThreadPool structure

---
+++ LabParalel3/ThreadPool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LabParalel3
+{
+   public class ThreadPool
+    {
+        private List<BlockingQueue<Action>> _queueList = new();
+        private List<Thread> _threadsList = new();
+        private int _threadsPerQueue;
+        private int _amountOfQueue;
+
+        public ThreadPool(int amountOfQueue = 3, int threadsPerQueue = 2)
+        {
+            _threadsPerQueue = threadsPerQueue;
+            _amountOfQueue = amountOfQueue;
+
+            if (amountOfQueue <= 0 || threadsPerQueue <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            for (int i = 0; i < amountOfQueue; i++)
+                _queueList.Add(new BlockingQueue<Action>());
+        }
+    }
+
+}
