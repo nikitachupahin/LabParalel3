455da7c9bfcda49d4672e5a9a31d5ddcb0a844de
nikitachupahin <nik123chupahin@gmail.com>
2025-03-22 17:19:35 +0200

    implemented BlockingQueue class

---
+++ LabParalel3/BlockingQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabParalel3
+{
+    public class BlockingQueue<T>
+    {
+        private Queue<T> _queue;
+        public int Count => _queue.Count;
+        public BlockingQueue()
+        {
+            _queue = new();
+        }
+        public void Enqueue(T item)
+        {
+            lock (_queue)
+            {
+                _queue.Enqueue(item);
+            }
+        }
+        public T Dequeue()
+        {
+            lock (_queue)
+            {
+                return _queue.Dequeue();
+            }
+        }
+    }
+}
